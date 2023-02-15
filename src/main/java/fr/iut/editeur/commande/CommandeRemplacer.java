9dfa907dfda80bff1587e6fbb7545fdb8cfbc891
Rayane Mallek <rayane.mallek@etu.umontpellier.fr>
2023-02-15 08:56:59 +0100

    Add CommandeRemplacer class

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,22 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;0;5;nouveauTexte");
+            return;
+        }
+        String texte = parameters[1];
+        this.document.remplacer(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]), parameters[3]);
+        super.executer();
+    }
+
+}
